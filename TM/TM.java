f496c48442e33f6ed045bfe7b6aaaae3e31c4c9c
1ninjwarrior <jadendawdy@gmail.com>
2024-04-18 11:44:32 -0600

    almost working code

--- TM/TM.java
+++ TM/TM.java
@@ -1,29 +1,33 @@
 package tm;
 
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class TM {
     private Map<Integer, TMState> states;
-    private int[] tape;
+    private List<Integer> tape;
     private int currentState;
     private int headPosition;
 
     public TM(Map<Integer, TMState> states, String inputString) {
         this.states = states;
-        this.tape = new int[1000]; // Increase the tape size to accommodate larger inputs
+        this.tape = new ArrayList<>(Collections.nCopies(1000, 0)); // Increase the tape size to accommodate larger inputs
         this.currentState = 0;
         this.headPosition = 500; // Start the head position in the middle of the tape
 
         // Initialize the tape with the input string
         for (int i = 0; i < inputString.length(); i++) {
-            tape[headPosition + i] = inputString.charAt(i) - '0';
+            set(headPosition + i, inputString.charAt(i) - '0');
         }
     }
 
+
     public void simulate() {
         while (currentState != states.size() - 1) { // While not in the halting state
-            int currentSymbol = tape[headPosition];
+            int currentSymbol = tape.get(headPosition);
             TMState.Transition transition = states.get(currentState).getTransition(currentSymbol);
 
             if (transition == null) {
@@ -31,13 +35,23 @@ public class TM {
             }
 
             // Update the tape
-            tape[headPosition] = transition.getWriteSymbol();
+            set(headPosition, transition.getWriteSymbol());
 
             // Move the head position
             if (transition.getDirection() == 'L') {
-                headPosition--;
+                if (headPosition == 0) {
+                    // If the head position is already at the start of the tape, expand the tape at the beginning
+                    tape.add(0, 0);
+                } else {
+                    headPosition--;
+                }
             } else {
                 headPosition++;
+                // Check if the head position is out of bounds of the current tape size
+                if (headPosition >= tape.size()) {
+                    // Expand the tape by adding a zero at the end
+                    tape.add(0);
+                }
             }
 
             // Update the current state
@@ -45,24 +59,28 @@ public class TM {
         }
     }
 
+    public void expandTape() {
+        tape.addAll(Collections.nCopies(1000, 0)); // Add 1000 more zeros to the end of the tape
+    }
+
     public void printTape() {
         StringBuilder sb = new StringBuilder();
         int startIndex = 0;
-        int endIndex = tape.length - 1;
+        int endIndex = tape.size() - 1;
         
         // Find the start index of non-zero symbols
-        while (startIndex < tape.length && tape[startIndex] == 0) {
+        while (startIndex < tape.size() && tape.get(startIndex) == 0) {
             startIndex++;
         }
         
         // Find the end index of non-zero symbols
-        while (endIndex >= 0 && tape[endIndex] == 0) {
+        while (endIndex >= 0 && tape.get(endIndex) == 0) {
             endIndex--;
         }
         
         // Print the tape content
         for (int i = startIndex; i <= endIndex; i++) {
-            sb.append(tape[i]);
+            sb.append(tape.get(i));
         }
         
         String tapeContent = sb.toString();
@@ -74,5 +92,17 @@ public class TM {
             sumOfSymbols += symbol - '0';
         }
         System.out.println("sum of symbols: " + sumOfSymbols);
+    }
+
+    public void set(int index, int value) {
+        // Check if the index is out of bounds of the current tape size
+        if (index >= tape.size()) {
+            // Calculate how many more elements we need
+            int additionalElementsNeeded = index - tape.size() + 1;
+            // Expand the tape by the number of additional elements needed
+            tape.addAll(Collections.nCopies(additionalElementsNeeded, 0));
+        }
+        // Set the value on the tape
+        tape.set(index, value);
     }
 }
